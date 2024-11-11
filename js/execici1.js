465e450019addaacb9c75d797cf0ed1cd6fbd88d
UsneMate <mmanzano@inscaparrella.cat>
2024-11-11 17:36:05 +0100

    vvvv

--- js/execici1.js
+++ js/execici1.js
@@ -1,12 +1,27 @@
 
+let cadena ="";
+let caracter = "";
+let novacadena = [];
+
 function procesarCadena(){
 
-    let cadena ="";
-    let caracter = "";
-
+    let contador = 0;
+    //guardem el contingut en variables, tant de la cadena de text com del caràcter
     cadena = document.getElementById("cadena").value;
     caracter = document.getElementById("caracter").value;
 
-    cadena.toLowerCase;
-    console.log(cadena);
+    //convertim la cadena de text a minuscules
+    cadena.toLowerCase();
+    //trec els espais a la cadena
+    cadena.split();
+
+    //recorro l'array i si trobo el caracter a la paraula en cada posició
+    //guardo la parual en una altra cadena
+    for (let i = 0; i < cadena.length; i++) {
+        if(cadena[i].search(caracter) == true){
+            novacadena = cadena[i].push;
+        }
+    }
+    console.log(novacadena);
+
 }
