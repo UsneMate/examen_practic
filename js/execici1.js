d53da8692d68c697f87b5970d94c66f1b3e1b033
UsneMate <mmanzano@inscaparrella.cat>
2024-11-11 16:51:42 +0100

    cc

---
+++ js/execici1.js
@@ -0,0 +1,12 @@
+
+function procesarCadena(){
+
+    let cadena ="";
+    let caracter = "";
+
+    cadena = document.getElementById("cadena").value;
+    caracter = document.getElementById("caracter").value;
+
+    cadena.toLowerCase;
+    console.log(cadena);
+}
